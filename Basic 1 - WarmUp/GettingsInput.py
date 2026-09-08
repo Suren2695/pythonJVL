8152b85f2bae207e1aca2908f090fba4f20f140c
Suren2695 <surensura46@gmail.com>
2026-09-08 18:10:48 +0530

    input method

---
+++ Basic 1 - WarmUp/GettingsInput.py
@@ -0,0 +1,5 @@
+#Input string - The prompt string, 
+#if given, is printed to standard output without a trailing newline before reading input.
+
+inputString = input("Enter the string : ")
+print("The entered string is = ", inputString)
