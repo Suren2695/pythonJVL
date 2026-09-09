765d1f20c57ccbcca97c2739411c7cc41ee96df2
Suren2695 <surensura46@gmail.com>
2026-09-09 22:51:23 +0530

    lsit basics

---
+++ Basic 1 - WarmUp/List.py
@@ -0,0 +1,24 @@
+#list
+
+#empty list 
+my_list = []
+
+#list of integers
+my_lis = [1,2,3,4]
+
+#list with mixed data types
+my_list1 = [1, "hello", 3.14]
+
+#Example1
+language = ["French", "German","English","Czech"]
+
+#Accessing first element
+print(language[0]) #French ( Indexing - first)
+
+#Accessing the 4 element
+print(language[3])
+
+#Changing the 4th value on the list 
+
+language[3] = "tamil"
+print(language)
