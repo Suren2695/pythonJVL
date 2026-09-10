cb49f6523e4f1e7abfc4988e41b1316f6a4722bd
Suren2695 <surensura46@gmail.com>
2026-09-10 11:21:57 +0530

    tuples

---
+++ Basic 1 - WarmUp/tuples.py
@@ -0,0 +1,11 @@
+#Tuples - only difference in tuples and list is, In tuples we cannot change the values in the tuples.
+
+language = ("French", "polish", "Czech", "German")
+
+
+print(language)
+print(language(-1))
+print(language(3))
+
+# del language  -----#delete the entire laguage variable 
+
