729b08d1daaf61def12dbbd52d1212c30ca4e9cb
Suren2695 <surensura46@gmail.com>
2026-09-11 02:06:14 +0530

    tuple advance

--- Basic 1 - WarmUp/7.0 tuples.py
+++ Basic 1 - WarmUp/7.0 tuples.py
@@ -4,8 +4,22 @@ language = ("French", "polish", "Czech", "German")
 
 
 print(language)
-print(language(-1))
-print(language(3))
+print(language[-1])
+print(language[3])
 
 # del language  -----#delete the entire laguage variable 
 
+#find the length of the tuples
+
+number = (10, 50, 20, 30, 80, 100, 90, 72 , 20, 50, 60, 20)
+print(len(number)) 
+
+#Count the occurances - how many times 20 appeared
+print(number.count(20))
+
+#Index of the element
+print(number.index(20))
+
+#Fidn the index of the element of 20 from the reverse order 
+print(len(number)-1-number[::-1].index(20))
+#first reversing the tuple and then minusing that with length -1
