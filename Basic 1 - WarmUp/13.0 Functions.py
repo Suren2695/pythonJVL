dc28878f74f913032acf807555ecb7b58e5e4ac4
Suren2695 <surensura46@gmail.com>
2026-09-17 22:30:46 +0530

    sum of two numbers

--- Basic 1 - WarmUp/13.0 Functions.py
+++ Basic 1 - WarmUp/13.0 Functions.py
@@ -5,3 +5,10 @@ def prin_line():
     print("The line is cool") 
 prin_line()
 #It is mandatory to call the function outside
+
+#Example 2 - sum of 2 numbers
+
+def add_numbers(a,b):
+    sum = a+b
+    print(sum)
+add_numbers(3,2)
