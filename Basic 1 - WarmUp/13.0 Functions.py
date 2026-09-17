35552de238ee17a86f0f5bb0bee055f470cbafe9
Suren2695 <surensura46@gmail.com>
2026-09-17 22:36:12 +0530

    eg2

--- Basic 1 - WarmUp/13.0 Functions.py
+++ Basic 1 - WarmUp/13.0 Functions.py
@@ -12,3 +12,12 @@ def add_numbers(a,b):
     sum = a+b
     print(sum)
 add_numbers(3,2)
+
+#Example 3 - Return the sum 
+
+def return_sum(a,b):
+    return a+b
+result = return_sum(3,5)
+print(result)
+    
+
