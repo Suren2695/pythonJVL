b4d1bf87910307ef8341a7252efbbfaf7afd8d9c
Suren2695 <surensura46@gmail.com>
2026-09-24 02:06:52 +0530

    phase 2 chat bot

--- python-chatbot/chatbot.py
+++ python-chatbot/chatbot.py
@@ -6,9 +6,23 @@ print("=============================================")
 
 name = input("What is your name ? ")
 print(f'Nice to meet you, {name}')
+print("type 'bye' to exit. \n")
 
-message = input("You : ")
-print(f'Bot: {message}')
+while True:
+    message = input("You : ")
+    message = message.lower()
+
+    if message == 'hello':
+        print(f'Bot: {message}')
+    elif message == 'how are you':
+        print(f"Bot : I'm fine {name} !! you ??")
+    elif message == 'what is your name':
+        print("Bot : I'm Python chatbot 1.1v ")
+    elif message =='bye':
+        print(f"Good Byee !! {name}")
+        break
+    else:
+        print("Bot: Sorry I don't understand your message")
 
 
 
