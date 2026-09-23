3249be4a591f9470755afa5b5805cd80a9c336fd
Suren2695 <surensura46@gmail.com>
2026-09-23 23:37:02 +0530

    more exercises

--- Basic 1 - WarmUp/14.0 Lambda.py
+++ Basic 1 - WarmUp/14.0 Lambda.py
@@ -8,6 +8,25 @@ and automatically returns the result of its expression.
 #Example 1
 #LAMBDA Function - basic
 
-sqr = lambda x: x*x
-
+sqr = lambda x: x**2
 print(sqr(5))
+
+#lambda parameter: expression
+#** = exponent / power
+
+#Example 2 - cube root
+sqr1 =  lambda x: x**3
+print(sqr1(2))
+
+#Example 3 - Check whether the number is positive or not 
+positive = lambda x: x>0
+print(positive(10)) #true
+print(positive(-1)) #false
+
+#Example 4 - Lambda with 2 inputs
+input =  lambda x,y : x + y
+print(input(3,2))
+
+#Example 5 - Fins the bigger number
+max = lambda x,y : x if x>y else y 
+print(max(10,34)) //34
