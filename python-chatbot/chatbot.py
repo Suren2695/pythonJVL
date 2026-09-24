4bd1af3e447fd2d78d2b145a3aa1ad7008c9d008
Suren2695 <surensura46@gmail.com>
2026-09-25 01:19:35 +0530

    chatbot into function

--- python-chatbot/chatbot.py
+++ python-chatbot/chatbot.py
@@ -1,25 +1,42 @@
-#Python Chatbot 
+#Python chatbot
 
-print(' ========== WELCOME TO THE CHATBOT ========== ')
-print("      >>>>>> 🤖 Python Chatbot <<<<<<          ")
-print("=============================================")
-
-name = input("What is your name ? ")
-print(f'Nice to meet you, {name}')
-print("type 'bye' to exit. \n")
-
-while True:
-    message = input("You : ")
+def get_response(message):
     message = message.lower()
 
-    if message == 'hello':
-        print(f'Bot: {message}')
-    elif message == 'how are you':
-        print(f"Bot : I'm fine {name} !! you ??")
-    elif message == 'what is your name':
-        print("Bot : I'm Python chatbot 1.1v ")
-    elif message =='bye':
-        print(f"Good Byee !! {name}")
-        break
+    if "hello" in message or "hi" in message:
+        return message
+    elif "how are you" in message:
+        return "I'm fine"
+    elif "what is your age" in message:
+        return "I am bot 1.1V."
+
+    elif "your name" in message:
+        return "I'm PythonBot."
+
+    elif "python" in message:
+        return "Python is a high-level programming language."
+
+    elif "thank" in message:
+        return "You're welcome!"
+
+    elif "bye" in message:
+        return "Goodbye! 👋"
     else:
-        print("Bot: Sorry I don't understand your message")
+        return "Sorry, I don't understand that yet."
+
+print("=================================")
+print("       🤖 Python Chatbot")
+print("=================================")
+
+name = input("What is your name? ")
+
+print(f"Nice to meet you, {name}!")
+print("Type 'bye' to exit.\n")
+
+
+while True:
+    message = input("You: ")
+    response = get_response(message)
+    print("Bot:", response)
+    if "bye" in message.lower():
+        break
